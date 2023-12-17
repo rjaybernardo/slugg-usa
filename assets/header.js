c4b3fd22d8e0ce9a1ae58eaa6d17489a68e36ad2
Raphael Jay Bernardo <raphaeljaybernardo@gmail.com>
2023-12-17 13:27:47 +0800

    theme pull header data added

--- assets/header.js
+++ assets/header.js
@@ -1,8 +1,8 @@
 window['ThemeSection_header'] = (count) => {
-  const dropdownCount = parseInt(count);
-  const menuOpen = {};
+  const dropdownCount = parseInt(count)
+  const menuOpen = {}
   for (let i = 0; i < dropdownCount; i++) {
-    menuOpen['menu' + i] = false;
+    menuOpen['menu' + i] = false
   }
 
   return {
@@ -11,174 +11,135 @@ window['ThemeSection_header'] = (count) => {
     headerIsSticky: false,
     scrollY: 0,
     navWrapped: false,
-    headerHeight: 0,
     get isStuck() {
-      return this.headerIsSticky && this.scrollY > this.headerHeight * 2;
+      return this.headerIsSticky && this.scrollY > 100
     },
     get menuIsActive() {
-      return !Object.keys(menuOpen).every((k) => !menuOpen[k]);
+      return !Object.keys(menuOpen).every((k) => !menuOpen[k])
     },
     mounted() {
-      initTeleport(this.$root);
-
-      Alpine.store('modals').register('nav', 'leftDrawer');
+      initTeleport(this.$root)
+      Alpine.store('modals').register('nav', 'leftDrawer')
 
       document.addEventListener('keyup', (event) => {
         if (event.key === 'Escape') {
-          if (this.searchOpen === false) {
-            for (let i = 0; i < dropdownCount; i++) {
-              if (this.menuOpen['menu' + i] === true) {
-                this.menuOpen['menu' + i] = false;
-                document.querySelector(`[aria-controls="menu${i}"]`).focus();
-              }
-            }
-          } else {
-            if (!document.getElementById('predictiveSearchResults')) {
-              document.body.dispatchEvent(
-                new CustomEvent('shapes:search:closebutton')
-              );
+          for (let i = 0; i < dropdownCount; i++) {
+            if (this.menuOpen['menu' + i] === true) {
+              this.menuOpen['menu' + i] = false
+              document.querySelector(`[aria-controls="menu${i}"]`).focus()
             }
           }
         }
-      });
+      })
 
-      document.body.addEventListener('shapes:search:closebutton', () => {
-        this.searchOpen = false;
+      document.body.addEventListener('label:search:closebutton', () => {
+        this.searchOpen = false
+      })
 
-        this.$nextTick(() => {
-          const searchToggleEl = this.$root.querySelector('[data-open-search]');
-
-          if (searchToggleEl) {
-            setTimeout(() => {
-              searchToggleEl.focus();
-            }, 300);
-          }
-        });
-      });
-
-      if (
-        this.$root.hasAttribute('data-sticky-header') &&
-        this.$root.hasAttribute(
-          'data-transparent-when-overlay-enabled-and-stuck'
-        )
-      ) {
-        this.setUpStickyDetection();
+      if (this.$root.hasAttribute('data-sticky-header')) {
+        this.setUpSticky()
       }
 
       //set header height
 
-      this.headerResizeFunctions();
+      this.headerResizeFunctions()
       this._debouncedHeaderFunctions = debounce(
         this.headerResizeFunctions.bind(this),
         300
-      );
+      )
 
-      window.addEventListener('resize', this._debouncedHeaderFunctions);
+      window.addEventListener('resize', this._debouncedHeaderFunctions)
       document.addEventListener('shopify:section:load', (e) => {
-        if (!e.target.querySelector('.site-header')) return;
-        this.headerResizeFunctions();
-      });
+        if (!e.target.querySelector('.site-header')) return
+        this.headerResizeFunctions()
+      })
+    },
+    logoLoaded() {
+      setTimeout(() => {
+        this.calculateHeaderHeight()
+      }, 100)
     },
     headerResizeFunctions() {
-      this.calculateHeaderHeight();
-      this.calculateNavWrap();
+      this.calculateHeaderHeight()
+      this.calculateNavWrap()
     },
-    setUpStickyDetection() {
-      const headerContainerEl = this.$root.closest('.shopify-section');
-
-      const sentinelEl = document.createElement('div');
-      sentinelEl.setAttribute('id', 'HeaderSentinel');
-
-      headerContainerEl.parentNode.insertBefore(sentinelEl, headerContainerEl);
-
+    calculateNavWrap() {
+      const wrappedItems = []
+      let prevItem = {}
+      let currItem = {}
+      if (this.$refs.navigation !== undefined) {
+        const items = this.$refs.navigation.children
+        for (var i = 0; i < items.length; i++) {
+          currItem = items[i].getBoundingClientRect()
+          if (prevItem && prevItem.top < currItem.top) {
+            wrappedItems.push(items[i])
+          }
+          prevItem = currItem
+        }
+        if (wrappedItems.length > 0) {
+          this.navWrapped = true
+        } else {
+          this.navWrapped = false
+        }
+      }
+    },
+    calculateHeaderHeight() {
+      document.documentElement.style.setProperty(
+        '--header-height',
+        `${document.getElementById('headerBorderWrap').clientHeight}px`
+      )
+    },
+    setUpSticky() {
+      const headerContainer = document.getElementById('shopify-section-header')
       const observer = new IntersectionObserver(
         (e) => {
           e.forEach((entry) => {
-            this.headerIsSticky = entry.intersectionRatio < 1 ? true : false;
-          });
+            this.headerIsSticky = entry.intersectionRatio < 1 ? true : false
+          })
         },
         {
           rootMargin: '-1px 0px 0px 0px',
           threshold: [1],
         }
-      );
-
-      observer.observe(sentinelEl);
-
-      let timer = null;
-
+      )
+      observer.observe(headerContainer)
+      let timer = null
       const checkScrollPosition = (e) => {
-        this.scrollY = window.scrollY;
+        this.scrollY = window.scrollY
         if (timer !== null) {
-          clearTimeout(timer);
+          clearTimeout(timer)
         }
         timer = setTimeout(() => {
-          this.scrollY = window.scrollY;
-        }, 150);
-      };
-
-      const _throttledScrollHandler = throttle(checkScrollPosition, 150);
-      document.addEventListener('scroll', _throttledScrollHandler);
-
-      this.scrollY = window.scrollY;
-    },
-    calculateNavWrap() {
-      const wrappedItems = [];
-      let prevItem = {};
-      let currItem = {};
-      if (this.$refs.navigation !== undefined) {
-        const items = this.$refs.navigation.children;
-        for (var i = 0; i < items.length; i++) {
-          currItem = items[i].getBoundingClientRect();
-          if (prevItem && prevItem.top < currItem.top) {
-            wrappedItems.push(items[i]);
-          }
-          prevItem = currItem;
-        }
-        if (wrappedItems.length > 0) {
-          this.navWrapped = true;
-        } else {
-          this.navWrapped = false;
-        }
+          this.scrollY = window.scrollY
+        }, 150)
       }
-    },
-    calculateHeaderHeight() {
-      this.headerHeight =
-        document.getElementById('headerBorderWrap').clientHeight;
-      document.documentElement.style.setProperty(
-        '--header-height',
-        `${this.headerHeight}px`
-      );
+      const _debounceScrollHandler = throttle(checkScrollPosition, 150)
+      document.addEventListener('scroll', _debounceScrollHandler)
+      this.scrollY = window.scrollY
     },
     openMenu(index) {
-      this.menuOpen['menu' + index] = true;
+      this.menuOpen['menu' + index] = true
     },
     focusOut(event, menu) {
       if (event.relatedTarget) {
         const dropdownParent = event.relatedTarget.closest(
           '[data-header-dropdown]'
-        );
+        )
         if (!dropdownParent) {
-          this.menuOpen[menu] = false;
+          this.menuOpen[menu] = false
         }
       }
     },
-    searchFocusOut() {
-      this.$nextTick(() => {
-        if (!this.$refs.searchFieldComponent.contains(document.activeElement))
-          this.searchOpen = false;
-      });
-    },
     closeSearch() {
-      this.searchOpen = false;
-      this.$refs.search.focus();
+      this.searchOpen = false
+      this.$refs.search.focus()
     },
     openSearch() {
-      this.searchOpen = true;
-      let input = document.querySelector('.header-search-input');
+      this.searchOpen = true
+      let input = document.querySelector('.header-search-input')
       setTimeout(() => {
-        input.focus();
-      }, 100);
+        input.focus()
+      }, 100)
     },
-  };
-};
+  }
+}
