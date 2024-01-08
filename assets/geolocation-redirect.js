725aa7877a0542dbe6f44651e1336292041825e0
rjaybernardo <raphaeljaybernardo@gmail.com>
2024-01-08 21:36:44 +0800

    geolocation redirect added in theme settings

---
+++ assets/geolocation-redirect.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+    const geolocationEnabled = {{ settings.enable_geolocation_redirect | json }};
+
+    if (geolocationEnabled) {
+        $.ajax({
+            url: 'https://ipapi.co/json/',
+            type: 'GET',
+            dataType: 'json',
+            success: (data) => {
+                const country = data.country;
+                const currentUrl = window.location.href;
+
+                if (country === 'AU' && !currentUrl.includes('www.slugg.co')) {
+                    window.location.href = 'https://www.slugg.com.au';
+                } else if (country === 'US' && !currentUrl.includes('www.slugg.com.au')) {
+                    window.location.href = 'https://www.slugg.co';
+                }
+                // No redirection for other countries
+            },
+            error: () => console.error('Error with IP Lookup'),
+        });
+    }
+});
